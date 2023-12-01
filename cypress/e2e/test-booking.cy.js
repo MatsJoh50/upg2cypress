6ef965fe40f7601603d39f6dfe7f748536346185
AnnaLenaO <annalenao@me.com>
2023-12-01 15:02:48 +0100

    changed to best practice 'contains' instead of 'get'

--- cypress/e2e/test-booking.cy.js
+++ cypress/e2e/test-booking.cy.js
@@ -2,6 +2,8 @@ describe('Test button challenge card', () => {
   it('clicking first "button"', () => {
     cy.visit('http://127.0.0.1:5501/index.html')
 
-    cy.get('button').click({multiple: true})
+    cy.contains('Take challenge').click({multiple: true})
+
+    cy.contains('Search').click()
   })
 })
