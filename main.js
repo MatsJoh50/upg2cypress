d9b68b5b66fe9ba0816c962172ad29c0d65aa4ae
Mats Johansson <matjoh50@gmail.com>
2023-12-03 00:21:59 +0100

    removed comments

--- main.js
+++ main.js
@@ -16,20 +16,6 @@ hamburgerMenuLinks.forEach(link => {
 });
 
 ///// FUNKTIONS \\\\\
-// function runOpenMenu() {
-//   queryHtmlEle.style.overflow = "hidden"
-//   runOpenAndClose("flex");
-// }
-
-// function runCloseMenu() {
-//   runOpenAndClose("none");
-//   queryHtmlEle.style.removeProperty("overflow");
-// }
-
-// function runOpenAndClose(property) {
-//   menuBg.style.display = property;
-//   mobileMenu.style.display = property;
-// }
 
 function onlineLink(){
   const onlines = document.querySelectorAll(".Online");
